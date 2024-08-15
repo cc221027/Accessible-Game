2a8ff59718e6da642a6afc5aeb77c97ece8a1b76
cc221027 <cc221027@fhstp.ac.at>
2024-08-15 22:17:51 +0200

    added lap mechanics

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -5,20 +5,19 @@ public class CharacterData: MonoBehaviour
 {
     public int index;
     public string characterName;
-    public int completedLaps = 0;
+    private int _completedLaps = 0;
     
     private TrackManager _trackManager;
 
-    private void Start()
+    private void Awake()
     {
         _trackManager = FindObjectOfType<TrackManager>();
     }
     
     public void CompleteLap()
     {
-        completedLaps++;
-        Debug.Log("LapsComplete: " + completedLaps);
-        if (completedLaps >= _trackManager.Laps)
+        _completedLaps++;
+        if (_completedLaps >= _trackManager.Laps)
         {
             _trackManager.EndRace(this);
         }
