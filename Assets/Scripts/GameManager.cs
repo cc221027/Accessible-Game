7598266330e5988bed3d2a3007f4c6befe47a604
cc221027 <cc221027@fhstp.ac.at>
2024-09-24 00:02:56 +0200

    Added full voiceover Action

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -13,6 +15,8 @@ public class GameManager : MonoBehaviour
     public int SelectedCharacterIndex { get; private set; } = -1;
     
     public int SelectedTrackIndex { get; private set; } = -1;
+
+    private bool _readScreenPressed;
 
     public string winner;
     public string endTime;
@@ -31,8 +35,15 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+     
+        
     }
-    
+
+    private void Update()
+    {
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -54,5 +65,26 @@ public class GameManager : MonoBehaviour
         SelectedCharacterIndex = -1;
         SelectedTrackIndex = -1;
     }
+    
+    private void OnReadUI(InputValue value)
+    {
+        StartCoroutine(ReadUIElements());
+    }
+
+    private IEnumerator ReadUIElements()
+    {
+        UAP_BaseElement[] uiElements = FindObjectsOfType<UAP_BaseElement>();
+
+        UAP_BaseElement[] sortedElements = uiElements.OrderBy(element => element.m_ManualPositionOrder).ToArray();
+
+        foreach (UAP_BaseElement element in sortedElements)
+        {
+            element.SelectItem();
+            
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+
     
 }
--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -7,9 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour, ISelectHandler
 {
-    private AudioSource _audioSource;
-    private bool _isSceneAudioPlaying;
-
     private enum ButtonAction
     {
         None,
@@ -23,38 +20,10 @@ public class MenuButtons : MonoBehaviour, ISelectHandler
     private void Start()
     {
         Button button = GetComponent<Button>();
-        _audioSource = GetComponent<AudioSource>();
         
         button.onClick.AddListener(HandleButtonClick);
-        
-        PlaySceneAudio();
+       
     }
-
-    private void PlaySceneAudio()
-    {
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        string audioFilePath = currentSceneName switch
-        {
-            "Main Menu" => "Audio/Menu/Main Menu",
-            "Character Selection" => "Audio/Menu/Select a Character",
-            "Track Selection" => "Audio/Menu/Select a Track",
-            _ => string.Empty
-        };
-
-     
-        AudioClip clip = Resources.Load<AudioClip>(audioFilePath);
-        if (clip != null)
-        {
-            _audioSource.clip = clip;
-            _audioSource.volume = 0.5f;
-            _audioSource.Play();
-            _isSceneAudioPlaying = true;
-            StartCoroutine(CheckIfSceneAudioIsDone()); 
-        }
-        
-    }
-
-    
 
     private void HandleButtonClick()
     {
@@ -81,100 +50,10 @@ public class MenuButtons : MonoBehaviour, ISelectHandler
             GameManager.Instance.LoadScene(sceneName);
         }
     }
-    
-    private IEnumerator CheckIfSceneAudioIsDone(string buttonName = null)
+
+    public void OnSelect(BaseEventData data)
     {
-        while (_audioSource.isPlaying) 
-        {
-            yield return null; 
-        }
-    
-        _isSceneAudioPlaying = false;
-
-        if (!string.IsNullOrEmpty(buttonName))
-        {
-            string audioFilePath = GetAudioFilePathForButton(buttonName);
-            if (!string.IsNullOrEmpty(audioFilePath))
-            {
-                AudioClip clip = Resources.Load<AudioClip>(audioFilePath);
-                if (clip != null)
-                {
-                    _audioSource.clip = clip;
-                    _audioSource.Play();
-                }
-            }
-        }
-    }
-
-    public void OnSelect(BaseEventData eventData)
-    {
-        if (_isSceneAudioPlaying)
-        {
-            StartCoroutine(CheckIfSceneAudioIsDone(gameObject.name));
-        }
-        else
-        {
-            string audioFilePath = GetAudioFilePathForButton(gameObject.name);
-            if (!string.IsNullOrEmpty(audioFilePath))
-            {
-                AudioClip clip = Resources.Load<AudioClip>(audioFilePath);
-                if (clip != null)
-                {
-                    if (clip.name == "Confirm Button")
-                    {
-                        StartCoroutine(WaitForSecondsCoroutine(3, clip));
-                      
-                    }
-                    else
-                    {
-                        _audioSource.clip = clip;
-                        _audioSource.Play();
-                    }
-              
-                }
-            }
-        }
-    }
-
-
-    private string GetAudioFilePathForButton(string buttonName)
-    {
-        switch (buttonName)
-        {
-            case "SinglePlayerBtn":
-                return "Audio/Menu/Single Mode";
-            case "MultiplayerBtn":
-                return "Audio/Menu/Multi Mode";
-            case "QuitBtn":
-                return "Audio/Menu/Quit";
-            case "MainMenuBtn":
-                return "Audio/Menu/Confirm Button";
-            case "Jimmy":
-                return "Audio/Menu/Character 1";
-            case "Officer Jenkins":
-                return "Audio/Menu/Character 2";
-            case "Russel":
-                return "Audio/Menu/Character 3";
-            case "Stella":
-                return "Audio/Menu/Character 4";
-            case "Track1":
-                return "Audio/Menu/Track 1";
-            case "Track2":
-                return "Audio/Menu/Track 2";
-            case "Track3":
-                return "Audio/Menu/Track 3";
-            case "Track4":
-                return "Audio/Menu/Track 4";
-            default:
-                return string.Empty;
-        }
-    }
-
-    private IEnumerator WaitForSecondsCoroutine(int seconds, AudioClip clip)
-    {
-        yield return new WaitForSeconds(seconds);
-        _audioSource.clip = clip;
-        _audioSource.Play();
+        gameObject.GetComponent<UAP_BaseElement>().SelectItem();
     }
 
 }
